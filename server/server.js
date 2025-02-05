8f312ea51fb98e6e21c03dd250f50935459595f0
bishwabandhu <bishwabandhuparmar06@gmail.com>
2025-02-05 11:08:18 +0530

    Package file updated

--- server/server.js
+++ server/server.js
@@ -5,17 +5,25 @@ const cors = require("cors");
 const app = express();
 const Port = process.env.PORT || 4000;
 const cookieParser = require("cookie-parser");
+const path =  require('path');
+
 // Connect to MongoDB
 const connectDB = require("./database/config");
 connectDB();
 
+const _dirname = path.resolve();
 
 // Enable CORS for frontend
+// app.use(cors({
+//     origin: "http://localhost:5173", // Allow frontend to make requests
+//     credentials: true // Allow cookies if needed
+//   }));
+
 app.use(cors({
-    origin: "http://localhost:5173", // Allow frontend to make requests
-    credentials: true // Allow cookies if needed
-  }));
-  
+    origin: true,
+    credentials: true
+}));
+
 const userRoutes = require("./routes/userRoutes");
 app.use(express.json());
 app.use(cookieParser());
@@ -23,8 +31,13 @@ app.use("/api/users", userRoutes);
 
 
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
+// app.get("/", (req, res) => {
+//     res.send("Hello World");
+// });
+
+app.use(express.static(path.join(_dirname, '/client/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(_dirname, 'client' , 'dist', 'index.html'));
 });
 
 app.listen(Port, () => {
